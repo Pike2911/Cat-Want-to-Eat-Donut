a224e4708e3fd363760f73173abb27f0e9f5689b
LAPTOP-78QD76RI\User <pannawish.hir63@dome.tu.ac.th>
2022-01-23 11:03:17 +0700

    Player Movement Falling Donut

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float Walkspeed = 4f;
+
+    private Rigidbody2D rb2D;
+
+    float HorizontalMove = 0f;
+
+    float x = 0;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * Walkspeed * Time.deltaTime;
+    }
+    private void FixedUpdate()
+    {
+        rb2D.MovePosition(new Vector2(transform.position.x + HorizontalMove, 0f));
+    }
+}
